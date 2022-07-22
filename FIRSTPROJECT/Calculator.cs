68c54643d1af950b1234f4d51b571d4d078f4cba
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-07-22 17:00:34 +0530

    //comment

---
+++ FIRSTPROJECT/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+     class Calculator
+    { 
+
+        static void Main(string[] args)
+        {
+            double num1,num2;
+            int choice;
+            Console.WriteLine("Welcome To The Calculator");
+            Console.WriteLine("Functions");
+            Console.WriteLine("1. Addition" +
+                              "2. Subtraction" +
+                              "3. Division" +
+                              "4.Multiplication");
+            Console.WriteLine("Enter your choice");
+            choice=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter the First Number");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Second Number");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("The sum of numbers is :" + (num1 + num2));
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("The subtraction of numbers is :" + (num1 - num2));
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("The Division of numbers is :" + (num1 / num2));
+                    }
+                    break ;
+                    case 4:
+                    {
+                        Console.WriteLine("The multiplication of numbers is :" + (num1 * num2));
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice");
+                    break;
+            }
+        }
+    }
+}
--- FIRSTPROJECT/Program.cs
+++ FIRSTPROJECT/Program.cs
@@ -1,5 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello, world!");
-Console.WriteLine("please do it fastly");
-int a = 45;
- Console.WriteLine(a);
+using System;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //float a=0.0f;
+            //Console.WriteLine("enter number");
+            //a=Single.Parse(Console.ReadLine());
+            //Console.WriteLine("float no " + a);
+            //string firstName = "John";
+            //string lastName = "Doe";
+            //string name = $"My full name is: {firstName} {lastName}";
+            //Console.WriteLine(name);
+            // Full name
+            string name = "John Doe";
+
+            // Location of the letter D
+            int charPos = name.IndexOf("D");
+
+            // Get last name
+            string lastName = name.Substring(charPos);
+
+            // Print the result
+            Console.WriteLine(lastName); 
+        }
+    }
+}
