daec70e2b9ecaf8d87753db71a4bc91234bf1ef8
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-07-27 17:17:23 +0530

    hi

---
+++ FIRSTPROJECT/List1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    internal class List1
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int>();
+            Console.WriteLine("Enter the no of numbers you want to enter");
+            int size=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter numbers");
+            for (int i=0; i<size; i++)
+            {
+                int no1 = Convert.ToInt32(Console.ReadLine());
+                number.Add(no1);
+            }
+            Console.WriteLine("Numbers you have entered");
+            foreach(int s in number)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+
