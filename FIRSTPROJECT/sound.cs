1d8a09bbc98b81f924bd66cf27fadb01524a9636
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-07-28 19:16:04 +0530

    Interface use

---
+++ FIRSTPROJECT/sound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    interface Ianimal
+    {
+        public void sound();
+    }
+    class Lion : Ianimal
+    {
+        public void sound()
+        {
+            Console.WriteLine("Roar Roar !!");
+        }
+    }
+    class sound
+    {
+        public static void Main(string[] args)
+        {
+            Lion l1 = new Lion();
+            l1.sound();
+        }
+    }
+}
