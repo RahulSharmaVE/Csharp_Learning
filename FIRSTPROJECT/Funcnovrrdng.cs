c2542ceb58ff23643950ecc1c64047f8f4401de6
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-07-28 18:51:04 +0530

    Function Overriding

---
+++ FIRSTPROJECT/Funcnovrrdng.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    class Funcnovrrdng
+    {
+        public virtual void flower()
+        {
+            Console.WriteLine("Flower is White in Colour");
+        }
+    }
+    class Funcnovrrdnguse1 : Funcnovrrdng
+    {
+        public override void flower()
+        {
+            Console.WriteLine("Flower is Yellow in Colour");
+        }
+    }
+    class floweroverride
+    {
+        public static void Main(string[] args)
+        {
+            
+            Funcnovrrdng fn1 = new Funcnovrrdng();
+            Console.WriteLine("Function without Overriding : ");
+            Console.WriteLine();
+            fn1.flower();
+            Console.WriteLine();
+            Funcnovrrdnguse1 fn = new Funcnovrrdnguse1();
+            Console.WriteLine("Function After Overriding : ");
+            Console.WriteLine();
+            fn.flower();
+            Console.ReadLine();
+        }
+    }
+}
