b3edf1ec73c77cd8799c41a3bae1f55adbdc8bf1
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-01 19:47:48 +0530

    Private Constructor Implementation

---
+++ FIRSTPROJECT/Pvtconstuse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    class cnst
+    {
+        private cnst()
+        { }
+        public static void getTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+     class Pvtconstuse
+    {
+        static void Main(string[] args)
+        {
+            cnst.getTime();
+        }
+    }
+}
