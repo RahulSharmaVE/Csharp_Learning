92c04ec3b58efb8cc899d99c77c44ef876ebfdea
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-02 17:51:55 +0530

    Goto Statement example

---
+++ FIRSTPROJECT/Goto1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    internal class Goto1
+    {
+        static void Main(string[] args)
+        {
+        inelegible:
+            Console.WriteLine("You are not eligible for this Placement");
+
+            Console.WriteLine("Enter your percentage : ");
+            double per=Convert.ToDouble(Console.ReadLine());
+            if(per<60)
+            {
+                goto inelegible;
+            }
+            else
+            {
+                Console.WriteLine("We Welcome you Onboard !!!!");
+            }
+        }
+    }
+}
---
+++ FIRSTPROJECT/Serializableuse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    [Serializable]
+    class Serial
+    {
+        public int age;
+        public string name;
+        public Serial(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+    }
+     class Serializableuse
+    {
+        static void Main(string[] args)
+        {
+            string locn = @"E:\serializable\sample.pdf";
+            Serial s1 = new Serial(19, "Rahul Sharma");
+            FileStream fs = new FileStream(locn,FileMode.OpenOrCreate);
+            BinaryFormatter bf1 = new BinaryFormatter();
+            bf1.Serialize(fs, s1);
+            fs.Close();
+            Console.WriteLine("File Created Successfully !!");
+            Console.ReadLine();
+
+        }
+    }
+}
