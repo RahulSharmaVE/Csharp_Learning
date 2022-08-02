84d2f7607115b7c4601a016a4622ec13a6c8cafe
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-02 11:15:57 +0530

    Static Constructor

---
+++ FIRSTPROJECT/StaticCNSuse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    class CNSstatic
+    {
+        public static double pie;
+        public static string name;
+        
+        static CNSstatic()
+        {
+            pie = 3.14;
+            name = "Rahul Sharma";
+            Console.WriteLine("Static Constructor Invoked !!");
+        }
+        public CNSstatic()
+        {
+            Console.WriteLine("Defaukt Constructor Invoked !!");
+        }
+        public static void getDetails()
+        {
+            Console.WriteLine("pie value :" + pie );
+            Console.WriteLine("Name : " + name);
+        }
+    }
+     class StaticCNSuse
+    {
+        static void Main(string[] args)
+        {
+            CNSstatic ob1=new CNSstatic();
+            CNSstatic.getDetails();
+            Console.ReadLine();
+        }
+    }
+}
