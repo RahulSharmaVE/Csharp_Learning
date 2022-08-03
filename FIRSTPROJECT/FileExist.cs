0dd688b5fbb8d725fa80dc197abbf90f7ac53383
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-03 12:16:49 +0530

    File Handling - File Exists or not.

---
+++ FIRSTPROJECT/FileExist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FIRSTPROJECT
+{
+     class FileExist
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter File  to find ");
+            //string path = @"E:\serializable\sample.txt"; //verbatim literal
+            string path = Console.ReadLine();
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File Exist !!");
+            }
+            else
+            {
+                Console.WriteLine("File does'nt Exist !!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
