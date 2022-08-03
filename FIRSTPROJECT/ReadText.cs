41f607a7fb201c34a582b6d2b664c6355e32cd2a
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-03 12:41:19 +0530

    File Handling - Read all the text from the file

---
+++ FIRSTPROJECT/ReadText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    class ReadText
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter File  to find ");
+            //string path = @"E:\serializable\sample.txt"; //verbatim literal
+            string path = Console.ReadLine();
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exist !!");
+                string data=File.ReadAllText(path);
+                Console.WriteLine(data);    
+            }
+            else
+            {
+                Console.WriteLine("File does'nt Exist !!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
