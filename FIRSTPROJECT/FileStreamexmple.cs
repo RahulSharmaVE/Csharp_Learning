53570469c6e9a4e6d44f20c505bb38f21bea187f
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-03 18:15:50 +0530

    File Handling - File Stream class

---
+++ FIRSTPROJECT/FileStreamexmple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+     class FileStreamexmple
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\file1.txt";
+            FileStream fs = new FileStream(path, FileMode.Append,FileAccess.Write);
+            Console.WriteLine("Enter what you want to write in the file");
+            string s1=Console.ReadLine();
+            byte[] writedata=Encoding.UTF8.GetBytes(s1);
+            fs.Write(writedata, 0, writedata.Length);
+            //fs.WriteByte(66);
+            Console.WriteLine("file Written");
+            fs.Close();
+        }
+    }
+}
