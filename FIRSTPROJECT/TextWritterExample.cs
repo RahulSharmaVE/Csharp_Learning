6510c8226c5feaa47a9cbe60553fb1ce25ac1023
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-04 12:02:59 +0530

    File Handlinhg - Text Writter Example

---
+++ FIRSTPROJECT/TextWritterExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FIRSTPROJECT
+{
+     class TextWritterExample
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Text.txt";
+            using(TextWriter tw=File.CreateText(path))
+            {
+                tw.WriteLine("hello world");
+                tw.WriteLine("how are you");
+            }
+            Console.WriteLine("Text Created Sucessfully");
+        }
+    }
+}
