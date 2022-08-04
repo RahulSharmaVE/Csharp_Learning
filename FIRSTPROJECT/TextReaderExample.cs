d37884c0abe488e8af8b4fdd271ec21bd4bf9f91
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-04 12:07:59 +0530

    File Handling - Text Reader Example

---
+++ FIRSTPROJECT/TextReaderExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+     class TextReaderExample
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Text.txt";
+            using (TextReader tr = File.OpenText(path))
+            {
+                Console.WriteLine(tr.ReadToEnd());
+            }
+            Console.WriteLine("Text Readed Successfully...............");
+        }
+    }
+}
