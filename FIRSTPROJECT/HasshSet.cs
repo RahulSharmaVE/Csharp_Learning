dbea2bf33b1e6a79667a16efee09a75416c0e203
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-04 17:48:38 +0530

    Hashset and Sortedset Example

--- FIRSTPROJECT/HasshSet.cs
+++ FIRSTPROJECT/HasshSet.cs
@@ -11,23 +11,27 @@ namespace FIRSTPROJECT
     {
         static void Main(string[] args)
         {
-            HashSet<string> hs1 = new HashSet<string>();
+            // HashSet<string> hs1 = new HashSet<string>();
+            //   Console.WriteLine("Hashset Example : ");
+            SortedSet<string> set = new SortedSet<string>();
+            Console.WriteLine("Sortedset Example : ");
             Console.WriteLine("Enter no of names you want to enter.....");
             int no=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter names below : ");
             for(int i = 0; i < no; i++)
             {
-                hs1.Add(Console.ReadLine());
+                set.Add(Console.ReadLine());
             }
             Console.WriteLine(  "names after enter is : ");
-            foreach(string s1 in hs1)
+            foreach(string s1 in set)
             {
                 Console.WriteLine(s1);
             }
             Console.WriteLine("Enter name which you want to delete :");
-            hs1.Remove(Console.ReadLine());
-            Console.WriteLine("Updated Hashset is..............");
-            foreach (string s in hs1) 
+            set.Remove(Console.ReadLine());
+            //Console.WriteLine("Updated Hashset is..............");
+            Console.WriteLine("Updated Sortedset is..............");
+            foreach (string s in set) 
             {
                 Console.WriteLine(s);
             }
