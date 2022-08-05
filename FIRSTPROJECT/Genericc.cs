369066e27a538bd944de97a2ad45e57a5b6cc906
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-05 12:31:01 +0530

    Generic Class

---
+++ FIRSTPROJECT/Genericc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace FIRSTPROJECT
+{
+    class Gen1
+    {
+        //public Gen1(T msg)
+        //{
+        //    Console.WriteLine(msg);
+        //}
+        public void message<T>(T msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("Generic Class Created Sucessfully");
+        }
+    }
+
+
+
+        class Genericc
+        {
+            static void Main(string[] args)
+            {
+                Gen1 gen = new Gen1();
+                //Gen1<int> genI = new Gen1<int>(101);
+                //Gen1<char> getCh = new Gen1<char>('I');
+                gen.message("Hello world");
+            gen.message('a');
+            gen.message(54541554);
+            Console.ReadLine();
+            }
+        }
+    }
