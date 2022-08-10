9f68ace7361ec627e9b88e13d0b177e3f6ffeb80
RahulSharmaVE <sh20rahulVE@gmail.com>
2022-08-10 19:51:50 +0530

    GUID

---
+++ FIRSTPROJECT/GUIDuse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTPROJECT
+{
+    class GUIDuse
+    {
+        static void Main(string[] args)
+        {
+            Guid g = Guid.NewGuid();
+            Console.WriteLine(g.ToString()); //gives  36 characters including hypens
+            Console.WriteLine(g.ToString("N"));//to remove Hypens from the O/P
+        }
+    }
+}
